92826917f4f0db27c168515135d5d1b0dc1641df
KhomotsoMashupye <motsomash2242@gmail.com>
2026-04-05 17:39:21 +0200

    Add the resource group, virtual network and subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,28 @@ terraform {
 provider "azurerm" {
   features {}
 }
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.project_name}-vnet"
+  address_space       = [var.vnet_cidr]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "${var.project_name}-public-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.public_subnet_cidr]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "${var.project_name}-private-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.private_subnet_cidr]
+}
